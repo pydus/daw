a761076d090fbe634f02d2c2dca731f274b53958
pydus <kristofferpudas@gmail.com>
2017-04-02 01:24:50 +0200

    Start design of modules

--- src/client/app.js
+++ src/client/app.js
@@ -5,11 +5,56 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import Signup from './signup';
 import Login from './login';
 
+const Module = (props) => {
+  return(
+    <div className="module">
+      <div className="volume"></div>
+      <div className="square">
+        <h1>{props.name}</h1>
+        <div className="effects">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ModuleSection = () => {
+  return (
+    <div className="modules">
+      <Module name="Master"/>
+      <Module name="A Test Module"/>
+      <Module name="Guitar"/>
+      <Module name="Vocals"/>
+      <Module name="Effects"/>
+      <div className="create"></div>
+    </div>
+  );
+};
+
+const SongPosition = () => {
+  return (
+    <div className="song-position">
+      <div className="bar">
+        <div className="position"></div>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
   return (
-    <div>
-      <h1>DAW</h1>
-    </div>
+    <main>
+      <SongPosition/>
+      <ModuleSection/>
+    </main>
   );
 };
 
