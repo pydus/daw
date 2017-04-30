9b03b379ff2e7f15c24af012266755ee8159886e
pydus <kristofferpudas@gmail.com>
2017-04-30 06:41:50 +0200

    Make waveform code more readable

--- src/client/components/playlist.js
+++ src/client/components/playlist.js
@@ -11,76 +11,97 @@ export default class Playlist extends React.Component {
     this.updateCanvas();
   }
 
-  static drawLine(fromX, fromY, toX, toY, ctx) {
+  resize(width) {
+    const canvas = this.refs.canvas;
+    canvas.width = width;
+    canvas.style.width = width + 'px';
+  }
+
+  resizeIfNeeded(segmentSectionWidth) {
+    if (segmentSectionWidth > this.props.width) {
+      this.resize(segmentSectionWidth);
+      this.props.onWiden(segmentSectionWidth);
+    } else {
+      this.resize(this.props.width);
+    }
+  }
+
+  drawLine(fromX, fromY, toX, toY) {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
     ctx.beginPath();
     ctx.moveTo(fromX, fromY);
     ctx.lineTo(toX, toY);
     ctx.stroke();
   }
 
-  static resize(width, canvas) {
-    canvas.width = width;
-    canvas.style.width = width + 'px';
+  drawSegment(n, width, value, padding) {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    ctx.strokeStyle = '#10b98a';
+    ctx.lineWidth = width - padding;
+    this.drawLine(
+      width * n, canvas.height / 2 - value * canvas.height,
+      width * n, canvas.height / 2 + value * canvas.height
+    );
+  }
+
+  drawSegments(
+    buffer,
+    numberOfSegments,
+    segmentWidth = 5,
+    segmentPadding = 1,
+    segmentScale = 1
+  ) {
+    const segmentSectionWidth = numberOfSegments * segmentWidth;
+    const stereo = buffer.numberOfChannels === 2;
+    const left = buffer.getChannelData(0);
+    const right = stereo ? buffer.getChannelData(1) : null;
+    const maxPointsPerSegment =
+      (numberOfSegments > left.length) ?
+      1 : Math.floor(left.length / numberOfSegments);
+    const pointsPerSegment = 100;
+    const step = Math.ceil(maxPointsPerSegment / pointsPerSegment);
+
+    this.resizeIfNeeded(segmentSectionWidth);
+
+    let sum = 0, s = 0;
+
+    for (let i = 0, j = 0; i < left.length; i += step, j += step) {
+      if (stereo) {
+        sum += (Math.abs(left[i]) + Math.abs(right[i])) / 2;
+      } else {
+        sum += Math.abs(left[i]);
+      }
+      if (j >= maxPointsPerSegment) {
+        const value = sum * step / maxPointsPerSegment;
+        this.drawSegment(s, segmentWidth, value * segmentScale, segmentPadding);
+        s++;
+        j = 0;
+        sum = 0;
+      }
+    }
+  }
+
+  drawWaveform(buffer, segmentDuration) {
+    const numberOfSegments = Math.ceil(1000 * buffer.duration / segmentDuration);
+    this.drawSegments(buffer, numberOfSegments, 5, 1, 1);
   }
 
   updateCanvas() {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext('2d');
-    const canvasWidth = 1024;
-    const canvasHeight = 720;
-    const originalWidth = canvas.offsetWidth;
 
-    if (originalWidth === 0) {
+    if (canvas.offsetWidth === 0) {
       return false;
     }
 
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     if (this.props.bufferSource && this.props.bufferSource.buffer) {
       const buffer = this.props.bufferSource.buffer;
-      const N_SEGMENTS = Math.ceil(buffer.duration);
-      const SEGMENT_WIDTH = 5; //Math.floor(canvasWidth / N_SEGMENTS);
-      const SEGMENT_SCALE = 1;
-
-      const segmentSectionWidth = N_SEGMENTS * SEGMENT_WIDTH;
-
-      if (segmentSectionWidth > this.props.width) {
-        Playlist.resize(segmentSectionWidth, canvas);
-        this.props.onWiden(segmentSectionWidth);
-      } else {
-        Playlist.resize(this.props.width, canvas);
-      }
-
-      ctx.strokeStyle = '#10b98a';
-      ctx.lineWidth = Math.floor(SEGMENT_WIDTH * 0.8);
-
-      const stereo = buffer.numberOfChannels === 2;
-      const left = buffer.getChannelData(0);
-      const right = stereo ? buffer.getChannelData(1) : null;
-      const MAX_POINTS_PER_SEGMENT = (N_SEGMENTS > left.length) ?
-        1 : Math.floor(left.length / N_SEGMENTS);
-
-      const POINTS_PER_SEGMENT = 100;
-      const STEP = Math.ceil(MAX_POINTS_PER_SEGMENT / POINTS_PER_SEGMENT);
-
-      for (let i = 0, j = 0, sum = 0, s = 0; i < left.length; i += STEP, j += STEP) {
-        if (stereo) {
-          sum += (Math.abs(left[i]) + Math.abs(right[i])) / 2;
-        } else {
-          sum += Math.abs(left[i]);
-        }
-        if (j >= MAX_POINTS_PER_SEGMENT) {
-          const height = sum * STEP / MAX_POINTS_PER_SEGMENT * canvasHeight;
-          Playlist.drawLine(
-            SEGMENT_WIDTH * s, canvasHeight / 2 - height * SEGMENT_SCALE,
-            SEGMENT_WIDTH * s, canvasHeight / 2 + height * SEGMENT_SCALE,
-            ctx
-          );
-          s++;
-          j = 0;
-          sum = 0;
-        }
-      }
+      const segmentDuration = 1000;
+      this.drawWaveform(buffer, segmentDuration);
     }
   }
 
