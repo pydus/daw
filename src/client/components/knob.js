60110cb98bf355e86e4203d3dfab48a0b8c837db
pydus <kristofferpudas@gmail.com>
2017-05-09 18:49:41 +0200

    Create range component

--- src/client/components/knob.js
+++ src/client/components/knob.js
@@ -1,50 +1,19 @@
 'use strict';
 import React from 'react';
+import Range from './range';
 
 export default class Knob extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: Number(props.default), lastCursor: 0};
+    this.state = {value: Number(props.default)};
     this.default = Number(props.default) || 0;
-    // percentage to add to the value per pixel moved by the cursor
-    this.step = props.step || 0.35;
     this.max = Number(props.max) || 100;
     this.min = Number(props.min) || 0;
-    this.reset = this.reset.bind(this);
-    this.onMouseDown = this.onMouseDown.bind(this);
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseUp = this.onMouseUp.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
 
-  reset() {
-    this.setState({value: this.default});
-  }
-
-  onMouseMove(e) {
-    const dy = this.state.lastCursor - e.clientY;
-    this.setState((prevState) => {
-      const interval = Math.abs(this.min) + Math.abs(this.max);
-      const stepValue = this.step * interval / 100;
-      let value = prevState.value + stepValue * dy;
-      value = (value > this.max) ? this.max : value;
-      value = (value < this.min) ? this.min : value;
-      return {value, lastCursor: e.clientY};
-    });
-  }
-
-  onMouseUp(e) {
-    window.removeEventListener('mousemove', this.onMouseMove);
-    window.removeEventListener('mouseup', this.onMouseUp);
-  }
-
-  onMouseDown(e) {
-    if (e.altKey) {
-      this.reset();
-    } else {
-      this.setState({lastCursor: e.clientY});
-      window.addEventListener('mousemove', this.onMouseMove);
-      window.addEventListener('mouseup', this.onMouseUp);
-    }
+  onChange(value) {
+    this.setState({value});
   }
 
   render() {
@@ -52,14 +21,17 @@ export default class Knob extends React.Component {
     const interval = Math.abs(this.max -this.min);
     const percentage = Math.round(100 * value / interval);
 
-    return (
-      <div
-        className={`knob progress-${percentage}`}
-        onMouseDown={this.onMouseDown}
-        onDoubleClick={this.reset}
+    return(
+      <Range
+        onChange={this.onChange}
+        max={this.max}
+        min={this.min}
+        default={this.default}
       >
-        <div className="label">{this.props.label}</div>
-      </div>
+        <div className={`knob progress-${percentage}`}>
+          <div className="label">{this.props.label}</div>
+        </div>
+      </Range>
     );
   }
 };
---
+++ src/client/components/range.js
@@ -0,0 +1,63 @@
+'use strict';
+import React from 'react';
+
+export default class Range extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: Number(props.default), lastCursor: 0};
+    this.default = Number(props.default) || 0;
+    // percentage to add to the value per pixel moved by the cursor
+    this.step = props.step || 0.35;
+    this.max = Number(props.max) || 100;
+    this.min = Number(props.min) || 0;
+    this.reset = this.reset.bind(this);
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseMove = this.onMouseMove.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+  }
+
+  reset() {
+    this.setState({value: this.default});
+    this.props.onChange(this.default);
+  }
+
+  onMouseMove(e) {
+    const dy = this.state.lastCursor - e.clientY;
+    this.setState((prevState) => {
+      const interval = Math.abs(this.min) + Math.abs(this.max);
+      const stepValue = this.step * interval / 100;
+      let value = prevState.value + stepValue * dy;
+      value = (value > this.max) ? this.max : value;
+      value = (value < this.min) ? this.min : value;
+      this.props.onChange(value);
+      return {value, lastCursor: e.clientY};
+    });
+  }
+
+  onMouseUp(e) {
+    window.removeEventListener('mousemove', this.onMouseMove);
+    window.removeEventListener('mouseup', this.onMouseUp);
+  }
+
+  onMouseDown(e) {
+    if (e.altKey) {
+      this.reset();
+    } else {
+      this.setState({lastCursor: e.clientY});
+      window.addEventListener('mousemove', this.onMouseMove);
+      window.addEventListener('mouseup', this.onMouseUp);
+    }
+  }
+
+  render() {
+    return (
+      <div
+        onMouseDown={this.onMouseDown}
+        onDoubleClick={this.reset}
+        style={{display: 'inline-block'}}
+      >
+        {this.props.children}
+      </div>
+    );
+  }
+};
