c45c6c1d9b64ed65660bd1beeb950ab342b3073c
pydus <kristofferpudas@gmail.com>
2018-07-21 13:39:49 +0200

    Move clip icon to component

--- src/client/components/panel.js
+++ src/client/components/panel.js
@@ -5,6 +5,20 @@ import {
   addClip,
 } from '../actions';
 
+const ClipIcon = () => (
+  <svg viewBox="0 0 120 120">
+    <line x1="0" x2="0" y1="55" y2="65" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="15" x2="15" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="30" x2="30" y1="35" y2="85" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="45" x2="45" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="60" x2="60" y1="40" y2="80" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="75" x2="75" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="90" x2="90" y1="35" y2="85" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="105" x2="105" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
+    <line x1="120" x2="120" y1="55" y2="65" strokeWidth="5" strokeLinecap="round"/>
+  </svg>
+);
+
 export default class Panel extends Component {
   constructor(props) {
     super(props);
@@ -32,19 +46,6 @@ export default class Panel extends Component {
     const isMaster = this.props.module.id === 0;
     const isDestination = this.props.module.sources.length > 0;
     const panelTagStyle = isDestination ? {background: this.props.module.color} : {};
-    const clipIcon = (
-      <svg viewBox="0 0 120 120">
-        <line x1="0" x2="0" y1="55" y2="65" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="15" x2="15" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="30" x2="30" y1="35" y2="85" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="45" x2="45" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="60" x2="60" y1="40" y2="80" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="75" x2="75" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="90" x2="90" y1="35" y2="85" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="105" x2="105" y1="25" y2="95" strokeWidth="5" strokeLinecap="round"/>
-        <line x1="120" x2="120" y1="55" y2="65" strokeWidth="5" strokeLinecap="round"/>
-      </svg>
-    );
 
     return (
       <div className="panel">
@@ -62,8 +63,7 @@ export default class Panel extends Component {
               style={panelTagStyle}
             ></div>
           }
-          {this.props.module.clips.length > 0 && clipIcon
-          }
+          {this.props.module.clips.length > 0 && <ClipIcon/>}
           {!isMaster && !isDestination &&
             <label>
               <input type="file" accept="audio/*" onChange={this.onSourceChange}/>
