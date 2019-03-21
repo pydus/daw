2d47651f7ba1e6d8e35edbf3e85dfc1eaff57eeb
Kristoffer <kristofferpudas@gmail.com>
2019-03-21 11:43:23 +0100

    Split Grid drawing code into several methods

--- src/client/components/grid.js
+++ src/client/components/grid.js
@@ -11,41 +11,55 @@ export default class Grid extends Component {
     this.draw();
   }
 
-  draw() {
-    const {maxFrequency, minFrequency} = this.props;
+  drawVerticalLine(x) {
     const canvas = this.canvas;
     const ctx = canvas.getContext('2d');
-    ctx.lineWidth = 1;
-    ctx.strokeStyle = '#1e2e4d';
     ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  };
 
-    const drawHorizontalLine = y => {
-      ctx.moveTo(0, y);
-      ctx.lineTo(canvas.width, y);
-    };
+  drawHorizontalLine(y) {
+    const canvas = this.canvas;
+    const ctx = canvas.getContext('2d');
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
 
-    const drawVerticalLine = x => {
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, canvas.height);
-    };
+  drawVerticalLines() {
+    const {maxFrequency, minFrequency} = this.props;
+    const canvas = this.canvas;
 
     for (let i = 10; i < maxFrequency; i *= 10) {
       for (let j = 1; j < 10; j++) {
         const freq = i * (j + 1);
         const ratio = getLogFrequencyRatio(freq, minFrequency, maxFrequency);
         const x = canvas.width * ratio;
-        drawVerticalLine(x);  
+        this.drawVerticalLine(x);
       }
     }
+  }
 
+  drawHorizontalLines() {
+    const canvas = this.canvas;
     const numberOfHorizontalSpaces = 2 * this.props.maxValue / this.valuesPerHorizontalLine;
     const numberOfHorizontalLines = numberOfHorizontalSpaces - 1;
 
     for (let i = 0; i < numberOfHorizontalLines; i++) {
-      drawHorizontalLine((i + 1) * canvas.height / numberOfHorizontalSpaces);
+      this.drawHorizontalLine((i + 1) * canvas.height / numberOfHorizontalSpaces);
     }
+  }
 
-    ctx.stroke();
+  draw() {
+    const canvas = this.canvas;
+    const ctx = canvas.getContext('2d');
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = '#1e2e4d';
+    this.drawVerticalLines();
+    this.drawHorizontalLines();
   }
 
   render() {
