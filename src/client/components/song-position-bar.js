3efae7acab15f988326962cce37a43566fb8d294
Kristoffer <kristofferpudas@gmail.com>
2019-03-24 12:02:18 +0100

    Use ref for song position bar

--- src/client/components/song-position-bar.js
+++ src/client/components/song-position-bar.js
@@ -5,31 +5,40 @@ import {setPosition, savePosition} from '../actions';
 export default class SongPositionBar extends Component {
   constructor(props) {
     super(props);
-    this.state = {rect: null};
+    this.state = {isSettingPosition: false};
     this.onMouseMove = this.onMouseMove.bind(this);
     this.onMouseDown = this.onMouseDown.bind(this);
     this.onMouseUp = this.onMouseUp.bind(this);
+    this.bar = null;
+  }
+
+  getRect() {
+    return this.bar.getBoundingClientRect();
+  }
+
+  getMousePositionInSong(e) {
+    const rect = this.getRect();
+    return this.props.song.beats *
+      (e.clientX - rect.left) / rect.width;
   }
 
   onMouseDown(e) {
-    const rect = e.currentTarget.getBoundingClientRect();
-    const position = this.props.song.beats * (e.clientX - rect.left) / rect.width;
-    this.setState({rect: rect});
+    const position = this.getMousePositionInSong(e);
+    this.setState({isSettingPosition: true});
     this.props.dispatch(setPosition(position));
     this.props.dispatch(savePosition(position));
   }
 
   onMouseMove(e) {
-    if (this.state.rect) {
-      const position = this.props.song.beats *
-        (e.clientX - this.state.rect.left) / this.state.rect.width;
+    if (this.state.isSettingPosition) {
+      const position = this.getMousePositionInSong(e);
       this.props.dispatch(setPosition(position));
       this.props.dispatch(savePosition(position));
     }
   }
 
   onMouseUp() {
-    this.setState({rect: null});
+    this.setState({isSettingPosition: false});
   }
 
   render() {
@@ -37,7 +46,7 @@ export default class SongPositionBar extends Component {
 
     return (
       <GlobalMouse up={this.onMouseUp} move={this.onMouseMove}>
-        <div className="bar" onMouseDown={this.onMouseDown}>
+        <div className="bar" ref={bar => this.bar = bar} onMouseDown={this.onMouseDown}>
           <div className="position" style={{width}}></div>
         </div>
       </GlobalMouse>
